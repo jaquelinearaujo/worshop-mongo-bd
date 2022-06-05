36e8388a558f884cce7326ac88f3daf2d0a318c4
jaquelinearaujo <jaquelineluiza4444@gmail.com>
2022-06-05 10:26:27 -0300

    main: Add simple query methods

--- src/main/java/com/javacourse/workshopmongo/respository/PostRepository.java
+++ src/main/java/com/javacourse/workshopmongo/respository/PostRepository.java
@@ -1,10 +1,13 @@
 package com.javacourse.workshopmongo.respository;
 
 import com.javacourse.workshopmongo.domain.Post;
-import com.javacourse.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
